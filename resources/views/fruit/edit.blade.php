541b6ea7c1e6b8ace5d8f4bd5a2e8e8354179111
Tom <tomah5714@gmail.com>
2018-03-21 10:38:35 +0300

    form update

--- resources/views/fruit/edit.blade.php
+++ resources/views/fruit/edit.blade.php
@@ -14,30 +14,30 @@
       
         <input name="_token" type="hidden" value="{{ csrf_token() }}">
         <div class="row">
-          <div class="col-md-4"></div>
+          <div class="col-md-3"></div>
           <div class="form-group col-md-4">
             <label for="name">Name:</label>
             <input type="text" class="form-control" name="fruit_name" value="{{$fruits->fruit_name}}">
           </div>
         </div>
         <div class="row">
-          <div class="col-md-4"></div>
+          <div class="col-md-3"></div>
             <div class="form-group col-md-4">
               <label for="quantity">Quantity:</label>
               <input type="int" class="form-control" name="quantity" value="{{$fruits->quantity}}">
             </div>
           </div>
-        </div>
+        
         <div class="row">
-          <div class="col-md-4"></div>
+          <div class="col-md-3"></div>
             <div class="form-group col-md-4">
               <label for="price">Price:</label>
               <input type="int" class="form-control" name="price" value="{{$fruits->price}}">
             </div>
           </div>
-        </div>
+        
         <div class="row">
-          <div class="col-md-4"></div>
+          <div class="col-md-3"></div>
             <div class="form-group col-md-4">
               <label for="discount">Discount:</label>
               <input type="int" class="form-control" name="discount" value="{{$fruits->discount}}">
